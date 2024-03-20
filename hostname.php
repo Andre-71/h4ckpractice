01dd7e1b635a6ee594e3c1fdf9b7715f048cd12b
Andre-71 <56390673+Andre-71@users.noreply.github.com>
2024-03-20 15:27:22 +0700

    Update hostname.php

--- hostname.php
+++ hostname.php
@@ -1,6 +1,3 @@
 <?php
-$hostname = getenv('HOSTNAME'); 
-if(!$hostname) $hostname = trim(`hostname`); 
-if(!$hostname) $hostname = exec('echo $HOSTNAME');
-if(!$hostname) $hostname = preg_replace('#^\w+\s+(\w+).*$#', '$1', exec('uname -a')); 
+print exec('hostname'); 
 ?>
